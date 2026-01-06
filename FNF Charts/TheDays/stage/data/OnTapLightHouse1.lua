905bc8d5795edb2d227ccaaa7429d0135daea6ea
Skinny mon <skinnie.mon@gmail.com>
2026-01-05 22:12:20 -0500

    removed
    such betrayal

--- FNF Charts/TheDays/stage/data/OnTapLightHouse1.lua
+++
@@ -1,5 +0,0 @@
-function onCreate()
-    makeLuaSprite('OnTapLightHouse1', 'OnTapLightHouse1', -870, -320);
-    addLuaSprite('OnTapLightHouse1');
-    scaleObject('OnTapLightHouse1', 1.6, 1.6);
-end
